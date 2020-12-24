45c7aaafc8908f32fef0da5cd6ff862ec22879a9
Tim-404 <tzw202623@yahoo.com>
2020-12-23 20:15:32 -0800

    2D Unity Practice
    stuff

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+class CollisionHandler : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D o)
+    {
+        if (o.tag == "particle")
+            Destroy(o.gameObject);
+    }
+}
