5541a02109a5a4fe96dd646e54dd546f84d7c46d
Tim-404 <76505169+Tim-404@users.noreply.github.com>
2020-12-23 21:32:58 -0800

    change with comments file
    more stuff

---
+++ Assets/Comments.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comments : MonoBehaviour
+{
+    public void GetCodeFromGit()
+    {
+        Debug.Log("fetch and update from master in github desktop");
+    }
+
+    public void SubmitCodeToGit()
+    {
+        Debug.Log("push to your branch and create pull request");
+    }
+
+    public void UpdateMaster()
+    {
+        Debug.Log("copy paste from the wanted files");
+        Debug.Log("jk, don't. ");
+    }
+}
